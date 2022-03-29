c28770749318c73bd88e32871fec1be396c5e23e
Yebin Chon <yebin.chon@princeton.edu>
2022-03-29 00:00:06 +0000

    adi without macros

--- polybench-3.1/adi/adi.c
+++ polybench-3.1/adi/adi.c
@@ -6,33 +6,33 @@
  * Web address: http://polybench.sourceforge.net
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <math.h>
 
-/* Include polybench common header. */
-#include "polybench.h"
 
 /* Include benchmark-specific header. */
 /* Default data type is double, default size is 10x1024x1024. */
-#include "adi.h"
+#define N 1024
+#define TSTEPS 50
 
 
 /* Array initialization. */
 static
 void init_array (int n,
-		 DATA_TYPE POLYBENCH_2D(X,N,N,n,n),
-		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
-		 DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+		 double X[n][n],
+		 double A[n][n],
+		 double B[n][n])
 {
   int i, j;
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++)
       {
-	X[i][j] = ((DATA_TYPE) i*(j+1) + 1) / n;
-	A[i][j] = ((DATA_TYPE) i*(j+2) + 2) / n;
-	B[i][j] = ((DATA_TYPE) i*(j+3) + 3) / n;
+	    X[i][j] = ((double) i*(j+1) + 1) / n;
+	    A[i][j] = ((double) i*(j+2) + 2) / n;
+	    B[i][j] = ((double) i*(j+3) + 3) / n;
       }
 }
 
@@ -41,15 +41,15 @@ void init_array (int n,
    Can be used also to check the correctness of the output. */
 static
 void print_array(int n,
-		 DATA_TYPE POLYBENCH_2D(X,N,N,n,n))
+		 double X[n][n])
 
 {
   int i, j;
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++) {
-      fprintf(stderr, DATA_PRINTF_MODIFIER, X[i][j]);
-      if ((i * N + j) % 20 == 0) fprintf(stderr, "\n");
+      fprintf(stderr, "%0.2lf ", X[i][j]);
+      if ((i * n + j) % 20 == 0) fprintf(stderr, "\n");
     }
   fprintf(stderr, "\n");
 }
@@ -60,9 +60,9 @@ void print_array(int n,
 static
 void kernel_adi(int tsteps,
 		int n,
-		DATA_TYPE POLYBENCH_2D(X,N,N,n,n),
-		DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
-		DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+		double X[n][n],
+		double A[n][n],
+		double B[n][n])
 {
   int t, i1, i2;
 
@@ -106,35 +106,30 @@ int main(int argc, char** argv)
   /* Retrieve problem size. */
   int n = N;
   int tsteps = TSTEPS;
+  int dump_code = atoi(argv[1]);
 
   /* Variable declaration/allocation. */
-  POLYBENCH_2D_ARRAY_DECL(X, DATA_TYPE, N, N, n, n);
-  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
-  POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, N, N, n, n);
+
+  double (*X)[n][n]; X = (double(*)[n][n])malloc(n*n*sizeof(double));
+  double (*A)[n][n]; A = (double(*)[n][n])malloc(n*n*sizeof(double));
+  double (*B)[n][n]; B = (double(*)[n][n])malloc(n*n*sizeof(double));
 
 
   /* Initialize array(s). */
-  init_array (n, POLYBENCH_ARRAY(X), POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
-
-  /* Start timer. */
-  polybench_start_instruments;
+  init_array (n, *X, *A, *B);
 
   /* Run kernel. */
-  kernel_adi (tsteps, n, POLYBENCH_ARRAY(X), 
-	      POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
+  kernel_adi (tsteps, n, *X, *A, *B);
 
-  /* Stop and print timer. */
-  polybench_stop_instruments;
-  polybench_print_instruments;
 
   /* Prevent dead-code elimination. All live-out data must be printed
      by the function call in argument. */
-  polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(X)));
+  if(dump_code == 1) print_array(n, *X);
 
   /* Be clean. */
-  POLYBENCH_FREE_ARRAY(X);
-  POLYBENCH_FREE_ARRAY(A);
-  POLYBENCH_FREE_ARRAY(B);
+  free((void*)X);
+  free((void*)A);
+  free((void*)B);
 
   return 0;
 }
