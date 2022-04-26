8c047b7aa170662ac0284bdc1e6f428a2ade9d97
SusanTan <zujunt@princeton.edu>
2022-04-26 14:34:03 -0400

    add NAS is

---
+++ IS/npbparams.h
@@ -0,0 +1,14 @@
+#define CLASS 'W'
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "26 Apr 2022"
+#define NPBVERSION "3.0"
+#define CC "cc"
+#define CFLAGS "-O"
+#define CLINK "cc"
+#define CLINKFLAGS "(none)"
+#define C_LIB "(none)"
+#define C_INC "(none)"
