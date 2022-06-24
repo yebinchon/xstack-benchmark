d2fe42cea7aed9bb9f44444c255fb709f39676e3
Yebin Chon <yebin.chon@princeton.edu>
2022-06-24 16:09:21 +0000

    for bleu score

--- polybench-pragma-inlined/main/3mm.c
+++ polybench-pragma-inlined/main/3mm.c
@@ -1,53 +1,45 @@
+int main(int argc, char** argv)
+{
 
-int main(int argc, char ** argv) {
-  uint32_t _argc = (uint32_t)argc;
-  uint8_t** _argv = (uint8_t**)argv;
-  uint64_t dump_code = strtol(_argv[1], ((uint8_t**)0), 10);
-  uint64_t ni = strtol(_argv[2], ((uint8_t**)0), 10);
-  uint64_t nj = strtol(_argv[3], ((uint8_t**)0), 10);
-  uint64_t nk = strtol(_argv[4], ((uint8_t**)0), 10);
-  uint64_t nl = strtol(_argv[5], ((uint8_t**)0), 10);
-  uint64_t nm = strtol(_argv[6], ((uint8_t**)0), 10);
-  uint8_t* E = malloc(nj * (ni << 3));
-  uint8_t* A = malloc(nk * (ni << 3));
-  uint8_t* B = malloc(nk * (nj << 3));
-  uint8_t* F = malloc(nl * (nj << 3));
-  uint8_t* C = malloc(nm * (nj << 3));
-  uint8_t* D = malloc((nl << 3) * nm);
-  uint8_t* G = malloc(nl * (ni << 3));
-for(uint64_t i = 0; i < ni;  i = i + 1){
-for(uint64_t j = 0; j < nk;  j = j + 1){
-  (((double*)A)+i * nk)[j] = (double)(i) * (double)(j) / (double)(ni);
-}
-}
-  if (dump_code == 1) {
-for(uint64_t i = 0; i < ni;  i = i + 1){
-for(uint64_t j = 0; j < nl;  j = j + 1){
-  fprintf(stderr, (_OC_str), (((double*)G)+i * nl)[j]);
-  if ((int)(i * ni + j) % (int)20 == 0) {
-  fputc(10, stderr);
-}
+  int dump_code = atoi(argv[1]);
+  int ni = atoi(argv[2]);
+  int nj = atoi(argv[3]);
+  int nk = atoi(argv[4]);
+  int nl = atoi(argv[5]);
+  int nm = atoi(argv[6]);
 
-}
-}
-  fputc(10, stderr);
-free(E);
-free(A);
-free(B);
-free(F);
-free(C);
-free(D);
-free(G);
+
+  double (*E)[ni][nj]; E = (double(*)[ni][nj])malloc((ni) * (nj) * sizeof(double));;
+  double (*A)[ni][nk]; A = (double(*)[ni][nk])malloc((ni) * (nk) * sizeof(double));;
+  double (*B)[nk][nj]; B = (double(*)[nk][nj])malloc((nk) * (nj) * sizeof(double));;
+  double (*F)[nj][nl]; F = (double(*)[nj][nl])malloc((nj) * (nl) * sizeof(double));;
+  double (*C)[nj][nm]; C = (double(*)[nj][nm])malloc((nj) * (nm) * sizeof(double));;
+  double (*D)[nm][nl]; D = (double(*)[nm][nl])malloc((nm) * (nl) * sizeof(double));;
+  double (*G)[ni][nl]; G = (double(*)[ni][nl])malloc((ni) * (nl) * sizeof(double));;
+
+  int i, j, k;
+
+  for (i = 0; i < ni; i++)
+    for (j = 0; j < nk; j++)
+      (*A)[i][j] = ((double) i*j) / ni;
+
+  if (dump_code == 1){
+  for (i = 0; i < ni; i++)
+    for (j = 0; j < nl; j++) {
+      fprintf (stderr, "%0.2lf ", (*G)[i][j]);
+      if ((i * ni + j) % 20 == 0) fprintf (stderr, "\n");
+    }
+  fprintf (stderr, "\n");
+  }
+
+
+  free((void*)E);;
+  free((void*)A);;
+  free((void*)B);;
+  free((void*)F);;
+  free((void*)C);;
+  free((void*)D);;
+  free((void*)G);;
+
   return 0;
 }
-
-free(E);
-free(A);
-free(B);
-free(F);
-free(C);
-free(D);
-free(G);
-  return 0;
-}
-
--- polybench-pragma-inlined/outlined/3mm.c
+++ polybench-pragma-inlined/outlined/3mm.c
@@ -1,73 +1,40 @@
-#pragma omp parallel 
-{
+#pragma omp for schedule(static)
+  for (i = 0; i < nk; i++)
+    for (j = 0; j < nj; j++)
+      (*B)[i][j] = ((double) i*(j+1)) / nj;
+#pragma omp for schedule(static)
+  for (i = 0; i < nj; i++)
+    for (j = 0; j < nm; j++)
+      (*C)[i][j] = ((double) i*(j+3)) / nl;
+#pragma omp for schedule(static)
+  for (i = 0; i < nm; i++)
+    for (j = 0; j < nl; j++)
+      (*D)[i][j] = ((double) i*(j+2)) / nk;
 
-#pragma omp for
-for(uint64_t i = 0; i<=(nk - 1);i+=1){
-for(uint64_t j = 0; j < nj;  j = j + 1){
-  *((double*)((B+(nj << 3) * i)+(j << 3))) = (double)(i) * (double)((j + 1)) / nj;
-}
-}
-}
-}
-  #pragma omp parallel 
-{
 
-#pragma omp for
-for(uint64_t i = 0; i<=(nj - 1);i+=1){
-for(uint64_t j = 0; j < nm;  j = j + 1){
-  *((double*)((C+(nm << 3) * i)+(j << 3))) = (double)(i) * (double)((j + 3)) / (double)(nl);
-}
-}
-}
-  #pragma omp parallel 
-{
+#pragma omp for schedule(static)
+  for (i = 0; i < ni; i++)
+    for (j = 0; j < nj; j++)
+    {
+      (*E)[i][j] = 0;
+      for (k = 0; k < nk; ++k)
+        (*E)[i][j] += (*A)[i][k] * (*B)[k][j];
+    }
 
-#pragma omp for
-for(uint64_t i = 0; i<=(nm - 1);i+=1){
-for(uint64_t j = 0; j < nl;  j = j + 1){
-  *((double*)((D+(nl << 3) * i)+(j << 3))) = (double)(i) * (double)((j + 2)) / (double)(nk);
-}
-}
-}
-  #pragma omp parallel 
-{
+#pragma omp for schedule(static)
+  for (i = 0; i < nj; i++)
+    for (j = 0; j < nl; j++)
+    {
+      (*F)[i][j] = 0;
+      for (k = 0; k < nm; ++k)
+        (*F)[i][j] += (*C)[i][k] * (*D)[k][j];
+    }
 
-#pragma omp for
-for(uint64_t i = 0; i<=(ni - 1);i+=1){
-for(uint64_t j = 0; j < nj;  j = j + 1){
-  *((double*)((E+(nj << 3) * i)+(j << 3))) = 0;
-  ((double*)E)[(i * nj + j)] = 0;
-for(uint64_t k = 0; k < nk;  k = k + 1){
-  ((double*)E)[(i * nj + j)] = (((double*)E)[(i * nj + j)] + *((double*)((A+(nk << 3) * i)+(k << 3))) * *((double*)((B+(j << 3))+(nj << 3) * k)));
-}
-}
-}
-}
-  #pragma omp parallel 
-{
-
-#pragma omp for
-for(uint64_t i = 0; i<=(nj - 1);i+=1){
-for(uint64_t j = 0; j < nl;  j = j + 1){
-  *((double*)((F+(nl << 3) * i)+(j << 3))) = 0;
-  ((double*)F)[(i * nl + j)] = 0;
-for(uint64_t k = 0; k < nm;  k = k + 1){
-  ((double*)F)[(i * nl + j)] = (((double*)F)[(i * nl + j)] + *((double*)((C+(nm << 3) * i)+(k << 3))) * *((double*)((D+(j << 3))+(nl << 3) * k)));
-}
-}
-}
-}
-  #pragma omp parallel 
-{
-
-#pragma omp for
-for(uint64_t i = 0; i<=(ni - 1);i+=1){
-for(uint64_t j = 0; j < nl;  j = j + 1){
-  *((double*)((G+(nl << 3) * i)+(j << 3))) = 0;
-  ((double*)G)[(i * nl + j)] = 0;
-for(uint64_t k = 0; k < nj;  k = k + 1){
-  ((double*)G)[(i * nl + j)] = (((double*)G)[(i * nl + j)] + *((double*)((E+(nj << 3) * i)+(k << 3))) * *((double*)((F+(j << 3))+(nl << 3) * k)));
-}
-}
-}
-}
+#pragma omp for schedule(static)
+  for (i = 0; i < ni; i++)
+    for (j = 0; j < nl; j++)
+    {
+      (*G)[i][j] = 0;
+      for (k = 0; k < nj; ++k)
+        (*G)[i][j] += (*E)[i][k] * (*F)[k][j];
+    }
