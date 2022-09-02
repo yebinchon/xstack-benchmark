ebc4abafe9b4d165dda95631fb688db01a0e11ca
Yebin Chon <yebin.chon@princeton.edu>
2022-09-02 14:43:03 -0400

    checks correctness of current dir

---
+++ scripts/check_correctness.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd $PWD
+rm correctness.txt
+for dir in */ ; do cd $dir ; make clean ; timeout 60 make check_correctness 
+  if [ $? -eq 0 ]; then
+    echo $dir is correct >> ../correctness.txt
+  else
+    echo $dir has failed >> ../correctness.txt
+  fi; cd ../ ; done
