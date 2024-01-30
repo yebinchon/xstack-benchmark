81a254c3c4fa9db1e5a678bb18fd2dabe77651e0
SusanTan <zujunt@princeton.edu>
2024-01-30 17:07:00 -0500

    jacobi-2d

--- polybench-cuda/jacobi-2d-imper/jacobi-2d-imper.c
+++ polybench-cuda/jacobi-2d-imper/jacobi-2d-imper.c
@@ -47,25 +47,19 @@ void print_array(int n,
 
 
 
-  static
-void kernel_jacobi_2d_imper(int tsteps,
-    int n,
-    double A[n][n],
-    double B[n][n])
-{
-  int t, i, j;
-
-  for (t = 0; t < tsteps; t++)
-  {
-    for (i = 1; i < n - 1; i++)
-      for (j = 1; j < n - 1; j++)
-        B[i][j] = 0.2 * (A[i][j] + A[i][j-1] + A[i][1+j] + A[1+i][j] + A[i-1][j]);
-    for (i = 1; i < n-1; i++)
-      for (j = 1; j < n-1; j++)
-        A[i][j] = B[i][j];
+static void kernel_jacobi_2d_imper(int tsteps, int n,
+                   double A[n][n], double B[n][n]) {
+  for (int t = 0; t < tsteps; t++) {
+    for (int i = 1; i < n - 1; i++)
+      for (int j = 1; j < n - 1; j++)
+        B[i][j] = (A[i][j] + A[i][j - 1] + A[i][1 + j] + A[1 + i][j] + A[i - 1][j]) / 5;
+    for (int i = 1; i < n - 1; i++)
+      for (int j = 1; j < n - 1; j++)
+        A[i][j] = (B[i][j] + B[i][j - 1] + B[i][1 + j] + B[1 + i][j] + B[i - 1][j]) / 5;
   }
-
 }
+
+
 
 
 int main(int argc, char** argv)
