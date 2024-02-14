e8f3192856d21b233e12641293ca91c7a1207029
SusanTan <zujunt@princeton.edu>
2024-02-13 23:23:33 -0500

    gemver

--- polybench-cuda/gemver/gemver.cu
+++ polybench-cuda/gemver/gemver.cu
@@ -62,21 +62,55 @@ __global__ void kernel_w(int n, double alpha, double beta,
 
 
 
-static void kernel(int n, double alpha, double beta,
+static void kernel(int n, double *alpha, double *beta,
                    double *A, double *u1, double *v1, double *u2, double *v2, double *w, double *x, double *y, double *z) {
+  double *dev_A;
+  double *dev_u1;
+  double *dev_v1;
+  double *dev_u2;
+  double *dev_v2;
+  double *dev_w;
+  double *dev_x;
+  double *dev_y;
+  double *dev_z;
+  double *dev_alpha;
+  double *dev_beta;
+  cudaMalloc(&dev_A, n*n*sizeof(double));
+  cudaMalloc(&dev_u1, n*sizeof(double));
+  cudaMalloc(&dev_v1, n*sizeof(double));
+  cudaMalloc(&dev_u2, n*sizeof(double));
+  cudaMalloc(&dev_v2, n*sizeof(double));
+  cudaMalloc(&dev_w, n*sizeof(double));
+  cudaMalloc(&dev_x, n*sizeof(double));
+  cudaMalloc(&dev_y, n*sizeof(double));
+  cudaMalloc(&dev_z, n*sizeof(double));
+  cudaMalloc(&dev_alpha, sizeof(double));
+  cudaMalloc(&dev_beta, sizeof(double));
+  cudaMemcpy(dev_A, A, n*n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_u1, u1, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_v1, v1, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_u2, u2, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_v2, v2, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_w, w, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_x, x, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_y, y, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_z, z, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_alpha, alpha, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_beta, beta, sizeof(double), cudaMemcpyHostToDevice);
   const unsigned threadsPerBlock = 256;
 
 
   {
     dim3 block(threadsPerBlock / 32, 32, 1);
     dim3 grid(num_blocks(n, block.x), num_blocks(n, block.y), 1);
-    kernel_A<<<grid, block>>>(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
+    kernel_A<<<grid, block>>>(n, *dev_alpha, *dev_beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
   }
 
 
-  kernel_x<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
-  kernel_y<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
-  kernel_w<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
+  kernel_x<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, *dev_alpha, *dev_beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
+  kernel_y<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, *dev_alpha, *dev_beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
+  kernel_w<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, *dev_alpha, *dev_beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
+  cudaMemcpy(w, dev_w, n*sizeof(double), cudaMemcpyDeviceToHost);
 }
 
 
@@ -165,44 +199,11 @@ int main(int argc, char** argv)
 	      y,
 	      z);
 
-  double *dev_A;
-  double *dev_u1;
-  double *dev_v1;
-  double *dev_u2;
-  double *dev_v2;
-  double *dev_w;
-  double *dev_x;
-  double *dev_y;
-  double *dev_z;
-  double *dev_alpha;
-  double *dev_beta;
-  cudaMalloc(&dev_A, n*n*sizeof(double));
-  cudaMalloc(&dev_u1, n*sizeof(double));
-  cudaMalloc(&dev_v1, n*sizeof(double));
-  cudaMalloc(&dev_u2, n*sizeof(double));
-  cudaMalloc(&dev_v2, n*sizeof(double));
-  cudaMalloc(&dev_w, n*sizeof(double));
-  cudaMalloc(&dev_x, n*sizeof(double));
-  cudaMalloc(&dev_y, n*sizeof(double));
-  cudaMalloc(&dev_z, n*sizeof(double));
-  cudaMalloc(&dev_alpha, sizeof(double));
-  cudaMalloc(&dev_beta, sizeof(double));
-  cudaMemcpy(dev_A, A, n*n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_u1, u1, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_v1, v1, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_u2, u2, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_v2, v2, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_w, w, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_x, x, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_y, y, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_z, z, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_alpha, alpha, sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_beta, beta, sizeof(double), cudaMemcpyHostToDevice);
+
   
   /* Run kernel. */
-  kernel(n, *alpha, *beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
+  kernel(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
 
-  cudaMemcpy(w, dev_w, n*sizeof(double), cudaMemcpyDeviceToHost);
   /* Prevent dead-code elimination. All live-out data must be printed
      by the function call in argument. */
   if(dump_code == 1) print_array(n, w);
