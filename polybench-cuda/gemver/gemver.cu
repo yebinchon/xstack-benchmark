8d06f0d0baa241f52a64db6cf5f36ae8fc1774a8
SusanTan <zujunt@princeton.edu>
2024-03-15 02:30:51 -0400

    gemver

--- polybench-cuda/gemver/gemver.cu
+++ polybench-cuda/gemver/gemver.cu
@@ -62,55 +62,22 @@ __global__ void kernel_w(int n, double alpha, double beta,
 
 
 
-static void kernel(int n, double *alpha, double *beta,
+static void kernel(int n, double alpha, double beta,
                    double *A, double *u1, double *v1, double *u2, double *v2, double *w, double *x, double *y, double *z) {
-  double *dev_A;
-  double *dev_u1;
-  double *dev_v1;
-  double *dev_u2;
-  double *dev_v2;
-  double *dev_w;
-  double *dev_x;
-  double *dev_y;
-  double *dev_z;
-  double *dev_alpha;
-  double *dev_beta;
-  cudaMalloc(&dev_A, n*n*sizeof(double));
-  cudaMalloc(&dev_u1, n*sizeof(double));
-  cudaMalloc(&dev_v1, n*sizeof(double));
-  cudaMalloc(&dev_u2, n*sizeof(double));
-  cudaMalloc(&dev_v2, n*sizeof(double));
-  cudaMalloc(&dev_w, n*sizeof(double));
-  cudaMalloc(&dev_x, n*sizeof(double));
-  cudaMalloc(&dev_y, n*sizeof(double));
-  cudaMalloc(&dev_z, n*sizeof(double));
-  cudaMalloc(&dev_alpha, sizeof(double));
-  cudaMalloc(&dev_beta, sizeof(double));
-  cudaMemcpy(dev_A, A, n*n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_u1, u1, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_v1, v1, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_u2, u2, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_v2, v2, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_w, w, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_x, x, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_y, y, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_z, z, n*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_alpha, alpha, sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_beta, beta, sizeof(double), cudaMemcpyHostToDevice);
+
   const unsigned threadsPerBlock = 256;
 
 
   {
     dim3 block(threadsPerBlock / 32, 32, 1);
     dim3 grid(num_blocks(n, block.x), num_blocks(n, block.y), 1);
-    kernel_A<<<grid, block>>>(n, *dev_alpha, *dev_beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
+    kernel_A<<<grid, block>>>(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
   }
 
 
-  kernel_x<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, *dev_alpha, *dev_beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
-  kernel_y<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, *dev_alpha, *dev_beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
-  kernel_w<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, *dev_alpha, *dev_beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
-  cudaMemcpy(w, dev_w, n*sizeof(double), cudaMemcpyDeviceToHost);
+  kernel_x<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
+  kernel_y<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
+  kernel_w<<<num_blocks(n, threadsPerBlock), threadsPerBlock>>>(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
 }
 
 
@@ -118,8 +85,6 @@ static void kernel(int n, double *alpha, double *beta,
 /* Array initialization. */
 static
 void init_array (int n,
-		 double *alpha,
-		 double *beta,
 		 double *A,
 		 double *u1,
 		 double *v1,
@@ -132,8 +97,7 @@ void init_array (int n,
 {
   int i, j;
 
-  *alpha = 43532;
-  *beta = 12313;
+
 
   for (i = 0; i < n; i++)
     {
@@ -173,8 +137,6 @@ int main(int argc, char** argv)
   int dump_code = atoi(argv[1]);
 
   /* Variable declaration/allocation. */
-  double *alpha = (double*)malloc(sizeof(double));
-  double *beta = (double*)malloc(sizeof(double));
   double *A = (double*)malloc(n*n*sizeof(double));
   double *u1 = (double*)malloc(n*sizeof(double));
   double *v1 = (double*)malloc(n*sizeof(double));
@@ -184,11 +146,11 @@ int main(int argc, char** argv)
   double *x = (double*)malloc(n*sizeof(double));
   double *y = (double*)malloc(n*sizeof(double));
   double *z = (double*)malloc(n*sizeof(double));
-  //__builtin_assume(n>0);
-  //__builtin_assume(n<0x7FFFFFFE);
 
+  double alpha = 43532;
+  double beta = 12313;
   /* Initialize array(s). */
-  init_array (n, alpha, beta,
+  init_array (n, 
 	      A,
 	      u1,
 	      v1,
@@ -200,9 +162,41 @@ int main(int argc, char** argv)
 	      z);
 
 
+  double *dev_A;
+  double *dev_u1;
+  double *dev_v1;
+  double *dev_u2;
+  double *dev_v2;
+  double *dev_w;
+  double *dev_x;
+  double *dev_y;
+  double *dev_z;
+  double *dev_alpha;
+  double *dev_beta;
+  cudaMalloc(&dev_A, n*n*sizeof(double));
+  cudaMalloc(&dev_u1, n*sizeof(double));
+  cudaMalloc(&dev_v1, n*sizeof(double));
+  cudaMalloc(&dev_u2, n*sizeof(double));
+  cudaMalloc(&dev_v2, n*sizeof(double));
+  cudaMalloc(&dev_w, n*sizeof(double));
+  cudaMalloc(&dev_x, n*sizeof(double));
+  cudaMalloc(&dev_y, n*sizeof(double));
+  cudaMalloc(&dev_z, n*sizeof(double));
+  cudaMalloc(&dev_alpha, sizeof(double));
+  cudaMalloc(&dev_beta, sizeof(double));
+  cudaMemcpy(dev_A, A, n*n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_u1, u1, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_v1, v1, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_u2, u2, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_v2, v2, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_w, w, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_x, x, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_y, y, n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_z, z, n*sizeof(double), cudaMemcpyHostToDevice);
   
   /* Run kernel. */
-  kernel(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z);
+  kernel(n, alpha, beta, dev_A, dev_u1, dev_v1, dev_u2, dev_v2, dev_w, dev_x, dev_y, dev_z);
+  cudaMemcpy(w, dev_w, n*sizeof(double), cudaMemcpyDeviceToHost);
 
   /* Prevent dead-code elimination. All live-out data must be printed
      by the function call in argument. */
--- polybench-cuda/gemver/gemver_reduced.cbe.c
+++ polybench-cuda/gemver/gemver_reduced.cbe.c
@@ -86,10 +86,10 @@ struct l_unnamed_1 {
 uint32_t cudaSetupArgument(uint8_t*, uint64_t, uint64_t);
 uint32_t cudaLaunch(uint8_t*);
 int main(int, char **) __ATTRIBUTELIST__((noinline));
-void _ZL10init_arrayiPdS_S_S_S_S_S_S_S_S_S_(uint32_t, double*, double*, double*, double*, double*, double*, double*, double*, double*, double*, double*) __ATTRIBUTELIST__((noinline, nothrow));
-void _ZL6kerneliPdS_S_S_S_S_S_S_S_S_S_(uint32_t, double*, double*, double*, double*, double*, double*, double*, double*, double*, double*, double*) __ATTRIBUTELIST__((noinline));
-void _ZL11print_arrayiPd(uint32_t, double*) __ATTRIBUTELIST__((noinline));
+void _ZL10init_arrayiPdS_S_S_S_S_S_S_S_(uint32_t, double*, double*, double*, double*, double*, double*, double*, double*, double*) __ATTRIBUTELIST__((noinline, nothrow));
 uint32_t cudaMemcpy(uint8_t*, uint8_t*, uint64_t, uint32_t);
+void _ZL6kerneliddPdS_S_S_S_S_S_S_S_(uint32_t, double, double, double*, double*, double*, double*, double*, double*, double*, double*, double*) __ATTRIBUTELIST__((noinline));
+void _ZL11print_arrayiPd(uint32_t, double*) __ATTRIBUTELIST__((noinline));
 uint32_t _ZL10num_blocksii(uint32_t, uint32_t) __ATTRIBUTELIST__((noinline, nothrow));
 uint32_t cudaConfigureCall(uint64_t, uint32_t, uint64_t, uint32_t, uint64_t, void*);
 uint32_t cudaMalloc(uint8_t**, uint64_t);
@@ -140,8 +140,6 @@ static __forceinline uint32_t llvm_urem_u32(uint32_t a, uint32_t b) {
 int main(int argc, char ** argv) {
   int32_t n;
   int32_t dump_code;
-  uint8_t* alpha;
-  uint8_t* beta;
   uint8_t* A;
   uint8_t* u1;
   uint8_t* v1;
@@ -151,11 +149,11 @@ int main(int argc, char ** argv) {
   uint8_t* x;
   uint8_t* y;
   uint8_t* z;
+  int32_t call62;
+  int32_t call89;
 
   n = atoi(argv[2]);
   dump_code = atoi(argv[1]);
-  alpha = malloc(8);
-  beta = malloc(8);
   A = malloc(n * n * 8);
   u1 = malloc(n * 8);
   v1 = malloc(n * 8);
@@ -165,10 +163,14 @@ int main(int argc, char ** argv) {
   x = malloc(n * 8);
   y = malloc(n * 8);
   z = malloc(n * 8);
-  _ZL10init_arrayiPdS_S_S_S_S_S_S_S_S_S_(n, ((double*)alpha), ((double*)beta), ((double*)A), ((double*)u1), ((double*)v1), ((double*)u2), ((double*)v2), ((double*)w), ((double*)x), ((double*)y), ((double*)z));
+  _ZL10init_arrayiPdS_S_S_S_S_S_S_S_(n, ((double*)A), ((double*)u1), ((double*)v1), ((double*)u2), ((double*)v2), ((double*)w), ((double*)x), ((double*)y), ((double*)z));
 ;
-  _ZL6kerneliPdS_S_S_S_S_S_S_S_S_S_(n, ((double*)alpha), ((double*)beta), ((double*)A), ((double*)u1), ((double*)v1), ((double*)u2), ((double*)v2), ((double*)w), ((double*)x), ((double*)y), ((double*)z));
+#pragma omp target data map(to: A[0:n * n * 8], u1[0:n * 8], v1[0:n * 8], u2[0:n * 8], v2[0:n * 8], x[0:n * 8], y[0:n * 8], z[0:n * 8]) map(tofrom: w[0:n * 8])
+{
+  _ZL6kerneliddPdS_S_S_S_S_S_S_S_(n, 43532, 12313, ((double*)A), ((double*)u1), ((double*)v1), ((double*)u2), ((double*)v2), ((double*)w), ((double*)x), ((double*)y), ((double*)z));
 ;
+
+}
   if (dump_code == 1) {
 _ZL11print_arrayiPd(n, ((double*)w));
   }
@@ -185,13 +187,11 @@ free(((uint8_t*)((double*)z)));
 }
 
 
-void _ZL10init_arrayiPdS_S_S_S_S_S_S_S_S_S_(uint32_t n, double* alpha, double* beta, double* A, double* u1, double* v1, double* u2, double* v2, double* w, double* x, double* y, double* z) {
+void _ZL10init_arrayiPdS_S_S_S_S_S_S_S_(uint32_t n, double* A, double* u1, double* v1, double* u2, double* v2, double* w, double* x, double* y, double* z) {
   int64_t i;
   uint64_t j;
 
-  *alpha = 43532;
-  *beta = 12313;
-#pragma omp parallel for 
+
 for(int64_t i = 0; i < n;   i = i + 1){
   u1[i] = (double)(i);
   u2[i] = (double)((i + 1) / n) / 2;
@@ -210,164 +210,123 @@ for(int64_t j = 0; j < n;   j = j + 1){
 }
 
 
-void _ZL6kerneliPdS_S_S_S_S_S_S_S_S_S_(uint32_t n, double* alpha, double* beta, double* A, double* u1, double* v1, double* u2, double* v2, double* w, double* x, double* y, double* z) {
+void _ZL6kerneliddPdS_S_S_S_S_S_S_S_(uint32_t n, double alpha, double beta, double* A, double* u1, double* v1, double* u2, double* v2, double* w, double* x, double* y, double* z) {
   struct l_struct_struct_OC_dim3 block;    /* Address-exposed local */
   struct l_struct_struct_OC_dim3 grid;    /* Address-exposed local */
   struct l_struct_struct_OC_dim3 agg_2e_tmp;    /* Address-exposed local */
-  struct l_struct_struct_OC_dim3 agg_2e_tmp62;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp4;    /* Address-exposed local */
   struct l_unnamed_1 agg_2e_tmp_2e_coerce;    /* Address-exposed local */
-  struct l_unnamed_1 agg_2e_tmp62_2e_coerce;    /* Address-exposed local */
-  struct l_struct_struct_OC_dim3 agg_2e_tmp64;    /* Address-exposed local */
-  struct l_struct_struct_OC_dim3 agg_2e_tmp66;    /* Address-exposed local */
-  struct l_unnamed_1 agg_2e_tmp64_2e_coerce;    /* Address-exposed local */
-  struct l_unnamed_1 agg_2e_tmp66_2e_coerce;    /* Address-exposed local */
-  struct l_struct_struct_OC_dim3 agg_2e_tmp71;    /* Address-exposed local */
-  struct l_struct_struct_OC_dim3 agg_2e_tmp73;    /* Address-exposed local */
-  struct l_unnamed_1 agg_2e_tmp71_2e_coerce;    /* Address-exposed local */
-  struct l_unnamed_1 agg_2e_tmp73_2e_coerce;    /* Address-exposed local */
-  struct l_struct_struct_OC_dim3 agg_2e_tmp78;    /* Address-exposed local */
-  struct l_struct_struct_OC_dim3 agg_2e_tmp80;    /* Address-exposed local */
-  struct l_unnamed_1 agg_2e_tmp78_2e_coerce;    /* Address-exposed local */
-  struct l_unnamed_1 agg_2e_tmp80_2e_coerce;    /* Address-exposed local */
-  uint8_t* dev_A;
-  uint8_t* dev_u1;
-  uint8_t* dev_v1;
-  uint8_t* dev_u2;
-  uint8_t* dev_v2;
-  uint8_t* dev_w;
-  uint8_t* dev_x;
-  uint8_t* dev_y;
-  uint8_t* dev_z;
-  uint8_t* dev_alpha;
-  uint8_t* dev_beta;
+  struct l_unnamed_1 agg_2e_tmp4_2e_coerce;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp6;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp8;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp6_2e_coerce;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp8_2e_coerce;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp13;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp15;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp13_2e_coerce;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp15_2e_coerce;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp20;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp22;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp20_2e_coerce;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp22_2e_coerce;    /* Address-exposed local */
+  int32_t call;
+  int32_t call3;
   uint8_t* _1;
   uint8_t* _2;
   uint8_t* _3;
   uint8_t* _4;
-  uint8_t* _5;
-  uint8_t* _6;
-  uint8_t* _7;
-  uint8_t* _8;
-  uint8_t* _9;
-  uint8_t* _10;
-  uint8_t* _11;
-  int32_t call59;
-  int32_t call61;
-  uint8_t* _12;
-  uint8_t* _13;
-  uint8_t* _14;
-  uint8_t* _15;
   uint32_t i;
   uint32_t j;
   uint32_t k;
   uint32_t l;
-  int32_t call65;
-  uint8_t* _16;
-  uint8_t* _17;
-  int32_t call72;
-  uint8_t* _18;
-  uint8_t* _19;
-  int32_t call79;
-  uint8_t* _20;
-  uint8_t* _21;
-  uint8_t* _22;
+  int32_t call7;
+  uint8_t* _5;
+  uint8_t* _6;
+  int32_t call14;
+  uint8_t* _7;
+  uint8_t* _8;
+  int32_t call21;
+  uint8_t* _9;
+  uint8_t* _10;
 
-  dev_A = malloc(n * n * 8);
-  dev_u1 = malloc(n * 8);
-  dev_v1 = malloc(n * 8);
-  dev_u2 = malloc(n * 8);
-  dev_v2 = malloc(n * 8);
-  dev_w = malloc(n * 8);
-  dev_x = malloc(n * 8);
-  dev_y = malloc(n * 8);
-  dev_z = malloc(n * 8);
-  dev_alpha = malloc(8);
-  dev_beta = malloc(8);
-  memcpy(((uint8_t*)((double*)dev_A)), ((uint8_t*)A), n * n * 8);
-  memcpy(((uint8_t*)((double*)dev_u1)), ((uint8_t*)u1), n * 8);
-  memcpy(((uint8_t*)((double*)dev_v1)), ((uint8_t*)v1), n * 8);
-  memcpy(((uint8_t*)((double*)dev_u2)), ((uint8_t*)u2), n * 8);
-  memcpy(((uint8_t*)((double*)dev_v2)), ((uint8_t*)v2), n * 8);
-  memcpy(((uint8_t*)((double*)dev_w)), ((uint8_t*)w), n * 8);
-  memcpy(((uint8_t*)((double*)dev_x)), ((uint8_t*)x), n * 8);
-  memcpy(((uint8_t*)((double*)dev_y)), ((uint8_t*)y), n * 8);
-  memcpy(((uint8_t*)((double*)dev_z)), ((uint8_t*)z), n * 8);
-  memcpy(((uint8_t*)((double*)dev_alpha)), ((uint8_t*)alpha), 8);
-  memcpy(((uint8_t*)((double*)dev_beta)), ((uint8_t*)beta), 8);
   block.field0 = 8;
   block.field1 = 32;
   block.field2 = 1;
-  call59 = _ZL10num_blocksii(n, block.field0);
-  call61 = _ZL10num_blocksii(n, block.field1);
-  grid.field0 = call59;
-  grid.field1 = call61;
+  call = _ZL10num_blocksii(n, block.field0);
+  call3 = _ZL10num_blocksii(n, block.field1);
+  grid.field0 = call;
+  grid.field1 = call3;
   grid.field2 = 1;
   memcpy(((uint8_t*)(&agg_2e_tmp)), ((uint8_t*)(&grid)), 12);
-  memcpy(((uint8_t*)(&agg_2e_tmp62)), ((uint8_t*)(&block)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp4)), ((uint8_t*)(&block)), 12);
   memcpy(((uint8_t*)(&agg_2e_tmp_2e_coerce)), ((uint8_t*)(&agg_2e_tmp)), 12);
-  memcpy(((uint8_t*)(&agg_2e_tmp62_2e_coerce)), ((uint8_t*)(&agg_2e_tmp62)), 12);
-#pragma omp parallel for 
-for(int32_t i = 0; i < call59;   i = i + 1){
+  memcpy(((uint8_t*)(&agg_2e_tmp4_2e_coerce)), ((uint8_t*)(&agg_2e_tmp4)), 12);
+#pragma omp target teams distribute parallel for
 
-for(int32_t j = 0; j < call61;   j = j + 1){
+for(int32_t i = 0; i < call;   i = i + 1){
+
+for(int32_t j = 0; j < call3;   j = j + 1){
 
 for(int32_t k = 0; k < 8;   k = k + 1){
 
 for(int32_t l = 0; l < 32;   l = l + 1){
-_Z8kernel_AiddPdS_S_S_S_S_S_S_S__OC_1(n, *((double*)dev_alpha), *((double*)dev_beta), ((double*)dev_A), ((double*)dev_u1), ((double*)dev_v1), ((double*)dev_u2), ((double*)dev_v2), ((double*)dev_w), ((double*)dev_x), ((double*)dev_y), ((double*)dev_z), call59, call61, 1, 8, 32, 1, i, j, 0, k, l, 0);
+_Z8kernel_AiddPdS_S_S_S_S_S_S_S__OC_1(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z, call, call3, 1, 8, 32, 1, i, j, 0, k, l, 0);
 }
 }
 }
 }
-  call65 = _ZL10num_blocksii(n, 256);
-  agg_2e_tmp64.field0 = call65;
-  agg_2e_tmp64.field1 = 1;
-  agg_2e_tmp64.field2 = 1;
-  agg_2e_tmp66.field0 = 256;
-  agg_2e_tmp66.field1 = 1;
-  agg_2e_tmp66.field2 = 1;
-  memcpy(((uint8_t*)(&agg_2e_tmp64_2e_coerce)), ((uint8_t*)(&agg_2e_tmp64)), 12);
-  memcpy(((uint8_t*)(&agg_2e_tmp66_2e_coerce)), ((uint8_t*)(&agg_2e_tmp66)), 12);
-#pragma omp parallel for 
-for(int32_t i = 0; i < call65;   i = i + 1){
+  call7 = _ZL10num_blocksii(n, 256);
+  agg_2e_tmp6.field0 = call7;
+  agg_2e_tmp6.field1 = 1;
+  agg_2e_tmp6.field2 = 1;
+  agg_2e_tmp8.field0 = 256;
+  agg_2e_tmp8.field1 = 1;
+  agg_2e_tmp8.field2 = 1;
+  memcpy(((uint8_t*)(&agg_2e_tmp6_2e_coerce)), ((uint8_t*)(&agg_2e_tmp6)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp8_2e_coerce)), ((uint8_t*)(&agg_2e_tmp8)), 12);
+#pragma omp target teams distribute parallel for
+
+for(int32_t i = 0; i < call7;   i = i + 1){
 
 for(int32_t j = 0; j < 256;   j = j + 1){
-_Z8kernel_xiddPdS_S_S_S_S_S_S_S__OC_2(n, *((double*)dev_alpha), *((double*)dev_beta), ((double*)dev_A), ((double*)dev_u1), ((double*)dev_v1), ((double*)dev_u2), ((double*)dev_v2), ((double*)dev_w), ((double*)dev_x), ((double*)dev_y), ((double*)dev_z), call65, 1, 1, 256, 1, 1, i, 0, 0, j, 0, 0);
+_Z8kernel_xiddPdS_S_S_S_S_S_S_S__OC_2(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z, call7, 1, 1, 256, 1, 1, i, 0, 0, j, 0, 0);
 }
 }
-  call72 = _ZL10num_blocksii(n, 256);
-  agg_2e_tmp71.field0 = call72;
-  agg_2e_tmp71.field1 = 1;
-  agg_2e_tmp71.field2 = 1;
-  agg_2e_tmp73.field0 = 256;
-  agg_2e_tmp73.field1 = 1;
-  agg_2e_tmp73.field2 = 1;
-  memcpy(((uint8_t*)(&agg_2e_tmp71_2e_coerce)), ((uint8_t*)(&agg_2e_tmp71)), 12);
-  memcpy(((uint8_t*)(&agg_2e_tmp73_2e_coerce)), ((uint8_t*)(&agg_2e_tmp73)), 12);
-#pragma omp parallel for 
-for(int32_t i = 0; i < call72;   i = i + 1){
+  call14 = _ZL10num_blocksii(n, 256);
+  agg_2e_tmp13.field0 = call14;
+  agg_2e_tmp13.field1 = 1;
+  agg_2e_tmp13.field2 = 1;
+  agg_2e_tmp15.field0 = 256;
+  agg_2e_tmp15.field1 = 1;
+  agg_2e_tmp15.field2 = 1;
+  memcpy(((uint8_t*)(&agg_2e_tmp13_2e_coerce)), ((uint8_t*)(&agg_2e_tmp13)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp15_2e_coerce)), ((uint8_t*)(&agg_2e_tmp15)), 12);
+#pragma omp target teams distribute parallel for
+
+for(int32_t i = 0; i < call14;   i = i + 1){
 
 for(int32_t j = 0; j < 256;   j = j + 1){
-_Z8kernel_yiddPdS_S_S_S_S_S_S_S__OC_3(n, *((double*)dev_alpha), *((double*)dev_beta), ((double*)dev_A), ((double*)dev_u1), ((double*)dev_v1), ((double*)dev_u2), ((double*)dev_v2), ((double*)dev_w), ((double*)dev_x), ((double*)dev_y), ((double*)dev_z), call72, 1, 1, 256, 1, 1, i, 0, 0, j, 0, 0);
+_Z8kernel_yiddPdS_S_S_S_S_S_S_S__OC_3(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z, call14, 1, 1, 256, 1, 1, i, 0, 0, j, 0, 0);
 }
 }
-  call79 = _ZL10num_blocksii(n, 256);
-  agg_2e_tmp78.field0 = call79;
-  agg_2e_tmp78.field1 = 1;
-  agg_2e_tmp78.field2 = 1;
-  agg_2e_tmp80.field0 = 256;
-  agg_2e_tmp80.field1 = 1;
-  agg_2e_tmp80.field2 = 1;
-  memcpy(((uint8_t*)(&agg_2e_tmp78_2e_coerce)), ((uint8_t*)(&agg_2e_tmp78)), 12);
-  memcpy(((uint8_t*)(&agg_2e_tmp80_2e_coerce)), ((uint8_t*)(&agg_2e_tmp80)), 12);
-#pragma omp parallel for 
-for(int32_t i = 0; i < call79;   i = i + 1){
+  call21 = _ZL10num_blocksii(n, 256);
+  agg_2e_tmp20.field0 = call21;
+  agg_2e_tmp20.field1 = 1;
+  agg_2e_tmp20.field2 = 1;
+  agg_2e_tmp22.field0 = 256;
+  agg_2e_tmp22.field1 = 1;
+  agg_2e_tmp22.field2 = 1;
+  memcpy(((uint8_t*)(&agg_2e_tmp20_2e_coerce)), ((uint8_t*)(&agg_2e_tmp20)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp22_2e_coerce)), ((uint8_t*)(&agg_2e_tmp22)), 12);
+#pragma omp target teams distribute parallel for
+
+for(int32_t i = 0; i < call21;   i = i + 1){
 
 for(int32_t j = 0; j < 256;   j = j + 1){
-_Z8kernel_widdPdS_S_S_S_S_S_S_S__OC_4(n, *((double*)dev_alpha), *((double*)dev_beta), ((double*)dev_A), ((double*)dev_u1), ((double*)dev_v1), ((double*)dev_u2), ((double*)dev_v2), ((double*)dev_w), ((double*)dev_x), ((double*)dev_y), ((double*)dev_z), call79, 1, 1, 256, 1, 1, i, 0, 0, j, 0, 0);
+_Z8kernel_widdPdS_S_S_S_S_S_S_S__OC_4(n, alpha, beta, A, u1, v1, u2, v2, w, x, y, z, call21, 1, 1, 256, 1, 1, i, 0, 0, j, 0, 0);
 }
 }
-  memcpy(((uint8_t*)w), ((uint8_t*)((double*)dev_w)), n * 8);
+  return;
 }
 
 
